1c481fc1a30f279ba3329af37fd03ab8a737632d
Scott Haselwood <scott.j.haselwood@gmail.com>
2014-06-16 08:55:26 -0700

    Add Theme files
    Initial upload of theme files

---
+++ lib/custom.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Custom functions
+ */
+
+/** Custom Post ==========================*/
+
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+	register_post_type( 'acme_product',
+		array(
+			'labels' => array(
+			'name' => __( 'Products' ),
+			'singular_name' => __( 'Product' )
+			),
+		'public' => true,
+		'has_archive' => true,
+		)
+	);
+}
---
+++ templates/header-top-navbar.php
@@ -0,0 +1,23 @@
+<header class="banner navbar navbar-default navbar-static-top" role="banner">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <nav class="collapse navbar-collapse" role="navigation">
+      <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-justified'));
+        endif;
+      ?>
+    </nav>
+  </div>
+
+  <a class="navbar-brand" href="<?php echo home_url(); ?>/"><h1><?php bloginfo('name'); ?></h1></a>
+
+</header>
